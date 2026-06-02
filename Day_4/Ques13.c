5846c03b20c1c6f9c554c9889421efeb54b330e5
Bhoomika851 <bhu85123@gmail.com>
2026-06-03 00:21:23 +0530

    Your message

--- Day_4/Ques13.c
+++ Day_4/Ques13.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 int main(){
-    int n;
+    int n, t1, t2, temp;
     printf("Enter the number:");
     scanf("%d", &n);
+    t1 = 0;
+    t2 = 1;
+    printf("The fibonacci series is:");
+    for(int i=1; i<=n; i++){
+        printf("%d ", t1);
+        temp = t1 + t2;
+        t1 = t2;
+        t2 = temp;
+        
+    }
+    printf("\n"); 
     return 0;
 
 } 
