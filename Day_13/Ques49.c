4c89e0039dccbb0ba5812aa961e2f01871f7892b
Bhoomika851 <bhu85123@gmail.com>
2026-06-14 17:22:41 +0530

    To display an array

---
+++ Day_13/Ques49.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int arr[50], n;
+    printf("No. of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Your array is: ");
+    for(int i=0; i<n; i++){
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
