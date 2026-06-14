7584342e8dce4989eec2be729429dcd36611864b
Bhoomika851 <bhu85123@gmail.com>
2026-06-14 17:46:42 +0530

    To find  sum and average of an array

--- Day_13/Ques50.c
+++ Day_13/Ques50.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
 int main(){
     int arr[50], n, sum=0;
+    float avg;
     printf("No. of Element: ");
     scanf("%d", &n);
 
-    printf("Enter %d elements: ",n);
+    printf("Enter %d elements:\n",n);
     for (int i = 0; i < n; i++){
-        scanf("%d ", &arr[i]);
+        scanf("%d", &arr[i]);
     }
     for(int i=0; i<n; i++){
-        sum+= arr[i];
+        sum += arr[i];
     }
-    printf("The sum of elements is: %d", sum);
+    avg = sum/n;
+    printf("The sum of elements is: %d\n", sum);
+    printf("The average is %.2f\n", avg);
     return 0;
     
 }
