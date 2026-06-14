fa04ca9eb1c057bb4025cbbb42632c8c6449e8df
Bhoomika851 <bhu85123@gmail.com>
2026-06-14 15:30:34 +0530

    To write a fn for fibonacci series

--- Day_12/Ques47.c
+++ Day_12/Ques47.c
@@ -1,7 +1,19 @@
 #include<stdio.h>
 int num1=0, num2=1, num3;
-int fibonacci(int num){
-    while(num>0){
-        for(int i=1; i<=num; i++)
+void fibonacci(int num){
+    for(int i=1; i<=num; i++){
+        printf("%d ", num1);
+        num3 = num1+num2;
+        num1=num2;
+        num2=num3;
     }
+    printf("\n");
+}
+int main(){
+    int x;
+    printf("Enter the no.: ");
+    scanf("%d", &x);
+    
+    fibonacci(x);
+    return 0;
 }
