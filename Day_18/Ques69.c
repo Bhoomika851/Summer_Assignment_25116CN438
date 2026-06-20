a8d13d7e3d42d22ac3e3b53ff51b5fe286ea0a3f
Bhoomika851 <bhu85123@gmail.com>
2026-06-20 22:53:41 +0530

    Binary sort

---
+++ Day_18/Ques69.c
@@ -0,0 +1,28 @@
+//binary sort
+#include<stdio.h>
+int main(){
+    int n, temp, arr[20];
+    printf("Enter no. of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements: ", n);
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("The sorted array is ");
+    for(int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
